a0af2512cbe506221e911ada2f0e06740d48301c
kiralightyagami <crucisgamer98@gmail.com>
2025-07-14 16:13:35 +0530

    creating portfolio

---
+++ app/components/container.tsx
@@ -0,0 +1,10 @@
+import { cn } from "@/app/lib/utils";
+import React from "react";
+
+export const Container = ({ children, className }: { children: React.ReactNode, className?: string }) => {
+    return (
+        <div className={cn("max-w-4xl mx-auto bg-white dark:bg-black p-4 md:p-10", className)}>
+            {children}
+        </div>
+    )
+}
